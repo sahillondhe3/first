e4a33705eea22954cb79902299aae601f682f2ac
sahillondhe3 <129080224+sahillondhe3@users.noreply.github.com>
2023-08-12 12:54:37 +0530

    backend

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var server = express();
+var routes = require('./routes/routes');
+var mongoose = require('mongoose');
+const cors = require('cors');
+mongoose.set('strictQuery',true);
+mongoose.connect("mongodb://localhost:27017/est",{useNewUrlParser: true,  useUnifiedTopology: true },function checkDB(error)
+{
+    if(error)
+    {
+        console.log("errorr")
+    }
+    else
+    {
+        console.log("DB Connectedddd!!!!!!!!!!!")
+    }
+});
+ 
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+ 
+server.listen(8000,function check(error)
+{
+    if(error)
+    {
+        console.log("errorr")
+    }
+    else
+    {
+        console.log("startedddddd")
+    }
+});
