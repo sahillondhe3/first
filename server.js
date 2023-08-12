ba9e001e1b798e880c4fccaedd88b6d70d02943e
sahillondhe3 <129080224+sahillondhe3@users.noreply.github.com>
2023-08-12 16:08:10 +0530

    uri

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var routes = require('./routes/routes');
 var mongoose = require('mongoose');
 const cors = require('cors');
 mongoose.set('strictQuery',true);
-mongoose.connect("mongodb://localhost:27017/est",{useNewUrlParser: true,  useUnifiedTopology: true },function checkDB(error)
+/*mongoose.connect("mongodb+srv://londhesahil88:<test123>@cluster0.uqcxowj.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true,  useUnifiedTopology: true },function checkDB(error)
 {
     if(error)
     {
@@ -14,7 +14,18 @@ mongoose.connect("mongodb://localhost:27017/est",{useNewUrlParser: true,  useUni
     {
         console.log("DB Connectedddd!!!!!!!!!!!")
     }
-});
+ });*/
+ const uri="mongodb+srv://londhesahil88:KFjubgRM2Mah6cqB@cluster0.uqcxowj.mongodb.net/?retryWrites=true&w=majority"
+ mongoose 
+  .connect(
+    uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
  
 server.use(cors());
 server.use(express.json());
